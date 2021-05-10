43181f8fe476f8134f3defa8b596587f6c4c92de
jrodr236 <jrodr236@xtec.cat>
2021-05-10 13:33:27 -0700

    Fitxer d'exemple per les claus utilitzades a la base de dades

---
+++ app/logica/exemple-claus.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+Utilitzar aquest fitxer com a plantilla per al fitxer claus.py
+"""
+
+#Aquesta clau ha de ser *exactament* de 64 caràcters hexadecimals.
+CLAU_PER_ENCRIPTAR_CONTRASENYES = "****************************************************************"
+
+# Aquesta clau ha de coincidir amb la que s'ha utilitzat en crear la base de dades
+CLAU_USUARI_BASE_DE_DADES = '**********'
