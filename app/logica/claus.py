2c2b7e7115c3aa2cb7923b45973855663d0ae177
jrodr236 <jrodr236@xtec.cat>
2021-05-10 13:30:25 -0700

    Eliminat fitxer amb claus

--- app/logica/claus.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-# Aquesta clau ha de ser *exactament* de 64 caràcters hexadecimals.
-CLAU_PER_ENCRIPTAR_CONTRASENYES = "1ADC93812324CFE10E5221BF2EB84F9C57431B37555D6112D16CD5F96FFEF743"
-
-# Aquesta clau ha de coincidir amb la que s'ha utilitzat en crear la base de dades
-CLAU_USUARI_BASE_DE_DADES = 'qwerty776'
